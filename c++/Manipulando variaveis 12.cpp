e4583ee156be163b9425744e8e567b74cbeefe06
Carvalhvanilson <146585128+Carvalhvanilson@users.noreply.github.com>
2024-03-04 00:31:53 +0300

    Add files via upload

---
+++ c++/Manipulando variaveis 12.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+    using namespece std;
+int main(){
+int varInt=100;
+char c ='r';
+    double pFlutuante =5.99;
+//Manipulando variaveis
+    cout<<"O valor da varInt é:"<<varInt<<endl;
+    cout<<"O valor da variavel é:"<<c<<endl;
+    cout<<"O valor da variavel é:"<<pFlutuante<<endl;
+    cout<<endl;
+
+//CLASSIFICANDO AS VARIAS VEIS E OS BYTES das memorias ocupadas
+    cout<<"Memoria da variavel varInt:"<<sizeof<<(varInt)<<"bytes"<<endl;
+        cout<<"Memoria da variavel c:"<<sizeof(c)<<"bytes"<<endl;
+     cout<<"Memoria da variavel pFlutante:"<<sizeof(pFlutuante)<<"bytes"<<endl;
+
+
+
+ 
+    return 0;
+}
